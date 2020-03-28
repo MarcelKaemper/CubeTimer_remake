a6eec566cd4e91986f4edd04a3c547b1e4ba7204
Marcel Kaemper <marcelkaemper862@gmail.com>
2020-03-28 23:40:08 +0100

    Added simple java GUI

---
+++ src/main/java/dev/marcelkaemper/cubeTimer/GUI.java
@@ -0,0 +1,14 @@
+package dev.marcelkaemper.cubeTimer;
+
+import javax.swing.*;
+
+public class GUI {
+    public GUI() {
+        JFrame frame = new JFrame("Cubetimer");
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 300);
+
+        frame.setVisible(true);
+    }
+}
